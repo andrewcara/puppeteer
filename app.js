32e17271c0f6943f2b674cf62f713e48c25f7a69
andrewcara <64450522+andrewcara@users.noreply.github.com>
2022-08-11 22:12:31 -0400

    Scraping using the correct css selector

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const puppet = require('puppeteer')
+const fs = require("fs/promises")
 
 async function start(){
     const browser = await puppet.launch({headless: false})
     const page = await browser.newPage()
-    await page.goto("https://fantasy.premierleague.com/")
+    await page.goto("https://www.basketball-reference.com/leagues/NBA_2021_leaders.html")
     
-    await browser.close()
-}
+   
 
+
+const names = await page.evaluate(() =>{
+    return title =  document.querySelectorAll('.data_grid_box > table > tbody > tr.first_place > td.who > a').map(x => x.textContent);
+    //css selector ".data_grid_box > table > tbody > tr.first_place > td.who > a"
+    //.map(x=> x.textContent) return the text content from each array that is returned
+    //think of the seletorAll as returning a bunch of <a> tags. Need to access the text in these tags using Array()textContent
+
+})
+
+fs.writeFile("names.txt", names.join("\r\n"))
+
+
+await browser.close()
+
+}
 start()
+
